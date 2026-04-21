4b3e916a3517440b0cae88d79249b765f7a1be55
st140471 <st140471@student.spbu.ru>
2026-04-21 10:54:53 +0300

    docs: add sample comments

--- include/libnostd/libnostd.hpp
+++ include/libnostd/libnostd.hpp
@@ -2,7 +2,11 @@
 #define LIBNOSTD_HPP
 
 #include <string>
-
+/**
+ * @brief     Выводит строку в отдельной строке
+ *
+ * @param[in] string Строка текста 
+*/
 void println(const std::string& str);
 
 #endif
